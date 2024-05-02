f6e35aaa207292288b417b28101fb0ebed61386d
Hridyanshu <124202756+HRIDYANSHU054@users.noreply.github.com>
2024-05-02 19:38:56 +0000

    Added the script for automating names extraction

---
+++ backend/scripts/extract_names.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Error: Please provide a file name as an argument."
+  exit 1
+fi
+
+file="$1"
+
+if [ ! -f "$file" ]; then
+  echo "Error: File '$file' does not exist."
+  exit 1
+fi
+
+awk -F ',' '{ 
+  if ($4 ~ /@amazon.com/) {
+    print $2, $3
+  }
+}' "$file" > test.txt
+
