4646eed852b5d23c678f54125ccf1e1a3fc4c7d4
Hridyanshu <124202756+HRIDYANSHU054@users.noreply.github.com>
2024-05-02 20:07:17 +0000

    changes Solved 4

--- backend/scripts/extract_names.sh
+++ backend/scripts/extract_names.sh
@@ -12,10 +12,11 @@ if [ ! -f "$file" ]; then
   exit 1
 fi
 
-# awk -F ',' 'BEGIN {OFS=" "} { 
-#   if ($4 ~ /@amazon.com/) { 
-#     print $2, $3
-#   }
-# }' "$file" > output_names.txt
+awk -F ',' 'BEGIN {OFS=" "} { 
+  email = toLower($4)
+  if (email ~ /@amazon.com/) { 
+    print $2, $3
+  }
+}' "$file" > output_names.txt
 
 
